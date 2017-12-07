4e19768c4e40a9f648f6e56b7143480178d96820
Francis ANDRE <francis.andre.kampbell@orange.fr>
2017-12-07 07:10:12 +0100

    Refactoring
    Signed-off-by: zosrothko <zosrothko@orange.fr>

--- Foundation/include/Poco/Types.h
+++ Foundation/include/Poco/Types.h
@@ -10,7 +10,7 @@
 // Copyright (c) 2004-2006, Applied Informatics Software Engineering GmbH.
 // and Contributors.
 //
-// SPDX-License-Identifier:	BSL-1.0
+// SPDX-License-Identifier:    BSL-1.0
 //
 
 
@@ -19,7 +19,6 @@
 
 
 #include "Poco/Foundation.h"
-#include <cstdint>
 
 
 #define POCO_HAVE_INT64 1
@@ -27,139 +26,206 @@
 
 namespace Poco {
 
-
-typedef std::int8_t   Int8;
-typedef std::uint8_t  UInt8;
-typedef std::int16_t  Int16;
-typedef std::uint16_t UInt16;
-typedef std::int32_t  Int32;
-typedef std::uint32_t UInt32;
-
-
 #if defined(_MSC_VER)
-	//
-	// Windows/Visual C++
-	//
+    //
+    // Windows/Visual C++
+    //
 
-	typedef long long  Int64;
-	typedef long long UInt64;
-	#define  int64(c) c ## LL
-	#define uint64(c) c ## ULL
+    typedef signed char            Int8;
+    typedef unsigned char          UInt8;
+    typedef signed short           Int16;
+    typedef unsigned short         UInt16;
+    typedef signed int             Int32;
+    typedef unsigned int           UInt32;
+    typedef signed long long       Int64;
+    typedef unsigned long long     UInt64;
+    #define  int64(c) c ## LL
+    #define uint64(c) c ## ULL
 
-	#if defined(_WIN64)
-		#define POCO_PTR_IS_64_BIT 1
-		typedef std::int64_t  IntPtr;
-		typedef std::uint64_t UIntPtr;
-	#else
-		typedef std::int32_t  IntPtr;
-		typedef std::uint32_t UIntPtr;
-	#endif
+    #if defined(_WIN64)
+        #define POCO_PTR_IS_64_BIT 1
+        typedef Int64              IntPtr;
+        typedef UInt64             UIntPtr;
+    #else
+        typedef Int32              IntPtr;
+        typedef UInt32             UIntPtr;
+    #endif
 
 #elif defined(__GNUC__) || defined(__clang__)
-	//
-	// Unix/GCC/Clang
-	//
+    //
+    // Unix/GCC/Clang
+    //
 
-	#if defined(_WIN64)
-		#define POCO_PTR_IS_64_BIT 1
-		typedef std::int64_t  IntPtr;
-		typedef std::uint64_t UIntPtr;
-	#else
-		#if defined(__LP64__)
-			typedef long			 Int64;
-			typedef unsigned long	UInt64;
-			#define  int64(c) c ##  L
-			#define uint64(c) c ## UL
+    typedef signed char                    Int8;
+    typedef unsigned char                  UInt8;
+    typedef signed short                   Int16;
+    typedef unsigned short                 UInt16;
+    typedef signed int                     Int32;
+    typedef unsigned int                   UInt32;
 
-			typedef long			 IntPtr;
-			typedef unsigned long	UIntPtr;
-			#define POCO_PTR_IS_64_BIT 1
-			#define POCO_LONG_IS_64_BIT 1
-		#else
-			typedef long long			 Int64;
-			typedef unsigned long long	UInt64;
-			#define  int64(c) c ##  LL
-			#define uint64(c) c ## ULL
-			typedef int					 IntPtr;
-			typedef unsigned int		UIntPtr;
-		#endif
-	#endif
+    #if defined(_WIN64)
+        #define POCO_PTR_IS_64_BIT 1
+        typedef signed long long           Int64;
+        typedef unsigned long long         UInt64;
+        typedef Int64                      IntPtr;
+        typedef UInt64                     UIntPtr;
+        #define  int64(c) c ##  L
+        #define uint64(c) c ## UL
+    #else
+        #if defined(__LP64__)
+            typedef long                   Int64;
+            typedef unsigned long          UInt64;
+            #define  int64(c) c ##  L
+            #define uint64(c) c ## UL
+
+            typedef Int64                  IntPtr;
+            typedef UInt64                 UIntPtr;
+            #define POCO_PTR_IS_64_BIT 1
+            #define POCO_LONG_IS_64_BIT 1
+        #else
+            typedef long long              Int64;
+            typedef unsigned long long     UInt64;
+            #define  int64(c) c ##  LL
+            #define uint64(c) c ## ULL
+            typedef Int32                  IntPtr;
+            typedef UInt32                 UIntPtr;
+        #endif
+    #endif
 
 #elif defined(__DECCXX)
-	//
-	// Compaq C++
-	//
+    //
+    // Compaq C++
+    //
 
-	typedef Int64 IntPtr;
-	typedef UInt64 UIntPtr;
-	#define POCO_PTR_IS_64_BIT 1
-	#define POCO_LONG_IS_64_BIT 1
+    typedef signed char            Int8;
+    typedef unsigned char          UInt8;
+    typedef signed short           Int16;
+    typedef unsigned short         UInt16;
+    typedef signed int             Int32;
+    typedef unsigned int           UInt32;
+    typedef signed __int64         Int64;
+    typedef unsigned __int64       UInt64;
+    typedef signed long            IntPtr;
+    typedef unsigned long          UIntPtr;
+    #define POCO_PTR_IS_64_BIT 1
+    #define POCO_LONG_IS_64_BIT 1
+    #define  int64(c) c ##  L
+    #define uint64(c) c ## UL
 
 #elif defined(__HP_aCC)
-	//
-	// HP Ansi C++
-	//
+    //
+    // HP Ansi C++
+    //
 
-	#if defined(__LP64__)
-		#define POCO_PTR_IS_64_BIT 1
-		#define POCO_LONG_IS_64_BIT 1
-		typedef Int64 IntPtr;
-		typedef UInt64 UIntPtr;
-	#else
-		typedef Int32 IntPtr;
-		typedef UInt32 UIntPtr;
-	#endif
+    typedef signed char            Int8;
+    typedef unsigned char          UInt8;
+    typedef signed short           Int16;
+    typedef unsigned short         UInt16;
+    typedef signed int             Int32;
+    typedef unsigned int           UInt32;
+    typedef signed long            IntPtr;
+    typedef unsigned long          UIntPtr;
+    #if defined(__LP64__)
+        #define POCO_PTR_IS_64_BIT 1
+        #define POCO_LONG_IS_64_BIT 1
+        typedef signed long        Int64;
+        typedef unsigned long      UInt64;
+        #define  int64(c) c ##  L
+        #define uint64(c) c ## UL
+    #else
+        typedef signed long long   Int64;
+        typedef unsigned long long UInt64;
+        #define  int64(c) c ##  LL
+        #define uint64(c) c ## ULL
+    #endif
+    #if defined(__LP64__)
+        #define POCO_PTR_IS_64_BIT 1
+        #define POCO_LONG_IS_64_BIT 1
+        typedef Int64 IntPtr;
+        typedef UInt64 UIntPtr;
+    #else
+        typedef Int32 IntPtr;
+        typedef UInt32 UIntPtr;
+    #endif
 
 #elif defined(__SUNPRO_CC)
-	//
-	// SUN Forte C++
-	//
+    //
+    // SUN Forte C++
+    //
 
-	#if defined(__sparcv9)
-		#define POCO_PTR_IS_64_BIT 1
-		#define POCO_LONG_IS_64_BIT 1
-		typedef Int64 IntPtr;
-		typedef UInt64 UIntPtr;
-	#else
-		typedef Int32 IntPtr;
-		typedef UInt32 UIntPtr;
-	#endif
+    typedef signed char            Int8;
+    typedef unsigned char          UInt8;
+    typedef signed short           Int16;
+    typedef unsigned short         UInt16;
+    typedef signed int             Int32;
+    typedef unsigned int           UInt32;
+    typedef signed long            IntPtr;
+    typedef unsigned long          UIntPtr;
+    #if defined(__sparcv9)
+        #define POCO_PTR_IS_64_BIT 1
+        #define POCO_LONG_IS_64_BIT 1
+        typedef signed long        Int64;
+        typedef unsigned long      UInt64;
+        #define  int64(c) c ##  L
+        #define uint64(c) c ## UL
+    #else
+        typedef signed long long   Int64;
+        typedef unsigned long long UInt64;
+        #define  int64(c) c ##  LL
+        #define uint64(c) c ## ULL
+    #endif
 
 #elif defined(__IBMCPP__)
-	//
-	// IBM XL C++
-	//
+    //
+    // IBM XL C++
+    //
 
-	#if defined(__64BIT__)
-		#define POCO_PTR_IS_64_BIT 1
-		#define POCO_LONG_IS_64_BIT 1
-		typedef Int64 IntPtr;
-		typedef UInt64 UIntPtr;
-	#else
-		typedef Int32 IntPtr;
-		typedef UInt32 UIntPtr;
-	#endif
+    typedef signed char            Int8;
+    typedef unsigned char          UInt8;
+    typedef signed short           Int16;
+    typedef unsigned short         UInt16;
+    typedef signed int             Int32;
+    typedef unsigned int           UInt32;
+    typedef signed long            IntPtr;
+    typedef unsigned long          UIntPtr;
+    #if _MIPS_SZLONG == 64
+        #define POCO_PTR_IS_64_BIT 1
+        #define POCO_LONG_IS_64_BIT 1
+        typedef signed long        Int64;
+        typedef unsigned long      UInt64;
+        #define  int64(c) c ##  L
+        #define uint64(c) c ## UL
+    #else
+        typedef signed long long   Int64;
+        typedef unsigned long long UInt64;
+        #define  int64(c) c ##  LL
+        #define uint64(c) c ## ULL
+    #endif
 
 #elif defined(__sgi)
-	//
-	// MIPSpro C++
-	//
+    //
+    // MIPSpro C++
+    //
 
-	#if _MIPS_SZLONG == 64
-		#define POCO_PTR_IS_64_BIT 1
-		#define POCO_LONG_IS_64_BIT 1
-		typedef Int64 IntPtr;
-		typedef UInt64 UIntPtr;
-	#else
-		typedef Int32 IntPtr;
-		typedef UInt32 UIntPtr;
-	#endif
+    #if _MIPS_SZLONG == 64
+        #define POCO_PTR_IS_64_BIT 1
+        #define POCO_LONG_IS_64_BIT 1
+        typedef Int64 IntPtr;
+        typedef UInt64 UIntPtr;
+        #define  int64(c) c ##  L
+        #define uint64(c) c ## UL
+    #else
+        typedef Int32 IntPtr;
+        typedef UInt32 UIntPtr;
+        #define  int64(c) c ##  LL
+        #define uint64(c) c ## ULL
+    #endif
 
 #endif
 
 
 #if defined(POCO_PTR_IS_64_BIT) && (POCO_PTR_IS_64_BIT == 1)
-	#define POCO_64_BIT
+    #define POCO_64_BIT
 #endif
 
 
